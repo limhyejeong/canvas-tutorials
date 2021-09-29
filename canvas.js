7785392d5f4b45ba29a82fbe4b3af24cafd02413
limteractive <limteractive@gmail.com>
2021-09-29 18:40:42 +0900

    commit

---
+++ canvas.js
@@ -0,0 +1,11 @@
+let canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let c = canvas.getContext('2d');
+
+c.fillRect(100, 100, 100, 100);
+c.fillRect(400, 100, 100, 100);
+c.fillRect(300, 300, 100, 100);
+console.log(canvas);
